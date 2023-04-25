315342844e9829d860d44bc435e6ab9ac4237c21
Diana Arnao <dpatriciaarnao@gmail.com>
2023-04-25 13:30:47 +0000

    HTO - Configuration EC2 Strapi Develoment environment

--- config/database.ts
+++ config/database.ts
@@ -5,7 +5,7 @@ export default ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "database-peritajes.cq3juhddbgkj.us-east-1.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "database-peritajes"),
+      database: env("DATABASE_NAME", "peritajes"),
       user: env("DATABASE_USERNAME", "admin"),
       password: env("DATABASE_PASSWORD", "julidiana"),
     },
