3a73b284564d92195c7b6a88d855b343c42a5910
Diana Patricia Arnao Gonzalez <70480017+dpatriciaarnao@users.noreply.github.com>
2023-05-04 16:41:39 -0500

    delete table

--- src/api/calificacion-elemento-categoria/controllers/calificacion-elemento-categoria.ts
+++
@@ -1,7 +0,0 @@
-/**
- * calificacion-elemento-categoria controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::calificacion-elemento-categoria.calificacion-elemento-categoria');
--- src/api/calificacion-elemento-categoria/services/calificacion-elemento-categoria.ts
+++
@@ -1,7 +0,0 @@
-/**
- * calificacion-elemento-categoria service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::calificacion-elemento-categoria.calificacion-elemento-categoria');
